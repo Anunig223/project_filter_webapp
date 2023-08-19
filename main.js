ed5772e3cc4c81cc214cb1c6d66176846e0f74ff
Anunig223 <79616960+Anunig223@users.noreply.github.com>
2023-08-18 18:40:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+clownnose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png")
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("myfilterimage.png");
+
+}
